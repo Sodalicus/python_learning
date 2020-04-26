7dfac266f2f2a4d37661448f8d2534f6411f9ed2
Paweł <p.krzeminski@interia.eu>
2020-04-26 19:57:20 +0200

    ...

--- asteroids.py
+++ asteroids.py
@@ -4,7 +4,7 @@
 # Author: Paweł Krzemiński
 
 from tkinter import *
-import time
+import time, random
 
 WIDTH = 640
 HEIGHT = 480
@@ -17,29 +17,48 @@ class Application(Frame):
         self.myship = Ship(self.canvas, 1,1)
         self.create_widgets()
         self.master = master
+        self.sprites = []
 
     def create_canvas(self):
         self.canvas = Canvas(height = HEIGHT , width = WIDTH , background = "white")
-        self.canvas.grid(row = 0, column = 0)
+        self.canvas.grid(row = 0, column = 0, columnspan = 3)
 
 
     def create_widgets(self):
-        start_button = Button(text = "Start", command = self.move_sprite())
-        start_button.grid(row = 1, column = 0, sticky=W)
+        up_button = Button(text = "Up", command = self.myship.move_up)
+        up_button.grid(row = 1, column = 1, sticky = N)
+        left_button = Button(text = "Left", command = self.myship.move_left)
+        left_button.grid(row = 2, column = 0, sticky = E)
+        down_button = Button(text = "Down", command = self.myship.move_down)
+        down_button.grid(row = 2, column = 1, sticky = N)
+        right_button = Button(text = "Right", command = self.myship.move_right)
+        right_button.grid(row = 2, column = 2, sticky = W)
+        self.canvas.bind_all('<KeyPress-Up>', self.myship.move)
+        self.canvas.bind_all('<KeyPress-Down>', self.myship.move)
+        self.canvas.bind_all('<KeyPress-Left>', self.myship.move)
+        self.canvas.bind_all('<KeyPress-Right>', self.myship.move)
 
-    def move_sprite(self):
-        self.myship.move()
+    def add_sprite(self, sprite):
+        self.sprites.append(sprite)
+
+
 
     def main(self):
+        self.myship = Ship(self.canvas, 1, 1)
+        for i in range(10):
+            i = AsteroidLarge(self.canvas, 1, 1)
+            self.add_sprite(i)
+        self.add_sprite(self.myship)
         while True:
-            self.myship.move()
+            for sprite in self.sprites:
+                sprite.update()
             self.master.update_idletasks()
             self.master.update()
-            time.sleep(0.5)
+            time.sleep(0.01)
 
 
 class Sprite():
-    def __init__(self, x, y, canvas, dx=0, dy=0):
+    def __init__(self, canvas, x, y, dx=0, dy=0):
         """ Create  shape with given center x, y"""
         pass
         self.x = x
@@ -47,6 +66,14 @@ class Sprite():
         self.dx = dx
         self.dy = dy
         self.canvas = canvas
+
+    def update(self):
+        self.coords = self.canvas.coords(self.id)
+        if self.coords[0] < 0 or self.coords[2] > WIDTH:
+            self.dx = -self.dx
+        if self.coords[1] < 0 or self.coords[3] > HEIGHT:
+            self.dy = -self.dy
+        self.canvas.move(self.id, self.dx, self.dy)
 
     def move(self):
         pass
@@ -57,32 +84,61 @@ class Sprite():
     def coords(self):
         pass
 
+
+class Ship(Sprite):
+    def __init__(self, canvas, x, y):
+        super(Ship, self).__init__(canvas, x, y)
+        self.id = self.canvas.create_oval(100,100,125,125)
+
+    def move_up(self):
+        self.dy = -5
+
+    def move_down(self):
+        self.dy = 5
+
+    def move_left(self):
+        self.dx = -5
+
+    def move_right(self):
+        self.dx = 5
+
+    def move(self, event):
+        if event.keysym == "Up": self.dy = -5
+        elif event.keysym == "Down": self.dy = 5
+        elif event.keysym == "Left": self.dx = -5
+        elif event.keysym == "Right": self.dx = 5
+
+
+    def update(self):
+        self.canvas.move(self.id, self.dx, self.dy)
+        if self.dx > 0: self.dx -= 1
+        elif self.dx < 0: self.dx += 1
+
+        if self.dy > 0: self.dy -= 1
+        elif self.dy < 0: self.dy += 1
+
 class AsteroidLarge(Sprite):
     def __init__(self, canvas, x, y):
-        super(AsteroidLarge, self).__init__(self, canvas, x, y)
-        self.id = kj 
-
+        super(AsteroidLarge, self).__init__(canvas, x, y)
+        self.id = self.canvas.create_oval(0, 0, 50, 50, fill="", outline="black")
+        x1 = random.randrange(WIDTH/2)
+        y1 = random.randrange(HEIGHT/2)
+        self.canvas.coords(self.id, x1, y1, x1+50, y1+50)
+        self.dx = 1 #random.randrange(1, 4)
+        self.dy = 1 #random.randrange(1, 4)
 
 class AsteroidMedium(Sprite):
     def __init__(self, canvas, x, y):
-        super(AsteroidMedium, self).__init__(self, canvas, x, y)
+        super(AsteroidMedium, self).__init__(canvas, x, y)
         pass
 
 
 class AsteroidSmall(Sprite):
     def __init__(self, canvas, x, y):
-        super(AsteroidSmall, self).__init__(self, canvas, x, y)
+        super(AsteroidSmall, self).__init__(canvas, x, y)
         pass
 
 
-class Ship(Sprite):
-    def __init__(self, canvas, x, y):
-        super(Ship, self).__init__(self, canvas, x, y)
-        self.id = self.canvas.create_polygon(1,1,20,1,16,10, fill="", outline="black")
-        self.canvas.move(self.id, 200,300)
-
-    def move(self):
-        self.canvas.move(self.id, 5,5)
 root = Tk()
 root.title("Asteroids")
 app = Application(root)
