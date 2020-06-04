dcacdddf1f14404c6c4ce9f7f92d24e4721ec1fc
Sodalicus <p.krzeminski@interia.eu>
2020-06-04 22:01:46 +0200

    ch13

--- How_to_Think_Like_a_Computer_Scientist/chapter13/reverse_order.py
+++ How_to_Think_Like_a_Computer_Scientist/chapter13/reverse_order.py
@@ -2,13 +2,24 @@
 
 def reverse_order(infile):
     f = open(infile)
+    new_words = []
     words = f.readlines()
-    for idx, lines in enumerate(words):
-        lines = str(idx+1)+lines
+    for line in words:
+        new_words.append(line)
     print(words)
-    words.reverse()
     print()
     print()
-    print(words)
+    print(new_words)
 
-reverse_order("lorem.txt") 
+def reverse_order2(infile):
+    f = open(infile)
+    words = f.readlines()
+    new_words = []
+    for i in range(len(words)-1,-1,-1):
+        new_words.append(words[i])
+    print(words)
+    print()
+    print()
+    print(new_words)
+
+reverse_order("lorem_nbr.txt")
